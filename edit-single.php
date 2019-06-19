2509444e6619be0a9865eea3125d9d9944fc0ecc
nawa-ton <kungcheow@gmail.com>
2019-06-19 03:58:58 -0700

    Finish your-account, edit-single

--- edit-single.php
+++ edit-single.php
@@ -1,57 +1,97 @@
 <?php include "header.php"; ?>
 
-	<h2>Edit</h2>
+<?php
+	
+	include('connect.php');
+	include('phpfunction.php');
 
-	<p class="remark">Note: This is just placeholder. The actual data should correspond to the selected item from edit-delete.php. </p>
-	<!-- ********* Add message to confirm successful update ******** -->
+	$id = $_GET["sid"];
+	$userid = $_GET["user"];
 
-	<form id="updateForm" method="POST" action="update.php">
-		<p></p>
-		<div>
-			<label>Name</label>
-			<input type="text" name="CommonName" value = "placeholder">
-		</div>
-		<div>
-			<label>Species</label>
-			<input type="text" name="Species" value = "placeholder">
-		</div>
-		<div>
-			<label>Primary Color</label>
-			<input type="text" name="PrimaryColor" value = "placeholder">
-		</div>
-		<div>
-			<label>Rarity</label>
-			<input type="text" name="Rarity" value = "placeholder">
-		</div>
-		<div>
-			<label>Habitat</label>
-			<input type="text" name="Habitat" value = "placeholder">
-		</div>
+	if(isset($_POST['updateorganism'])){
 
-		<div>
-			<label>Sighting Location</label>
-			<input type="text" name="Location_Name" value = "placeholder">
-		</div>
-
-		<div>
-			<label>Diet</label>
-			<input type="text" name="Diet" value = "placeholder">
-		</div>
-
-		<div>
-			<label>Aggressiveness</label>
-			<input type="text" name="Aggressiveness" value = "placeholder">
-		</div>
-		<div>
-			<label>Health</label>
-			<input type="text" name="Health" value = "placeholder">
-		</div>
-
-		<div>
-			<label>Sound</label>
-			<input type="text" name="Sound" value = "placeholder">
-		</div>
+		$selectspecies = (!empty($_POST['selectspecies']) ? $connection->real_escape_string($_POST['selectspecies']) : false);  
+		$query = "update sighting_report set species='$selectspecies' where sid =" . $id;
+		$result = mysqli_query($connection, $query);
 		
-		<input class ="button" type="submit" value="Submit" name="editsubmit">
+			echo "<p class='confirmmsg'>Organism has been updated</p>";
+		
+	}
 
+	if(isset($_POST['updatelocation'])){
+
+		$selectlocation = (!empty($_POST['selectlocation']) ? $connection->real_escape_string($_POST['selectlocation']) : false);  
+		$query = "update sighting_report set locationName='$selectlocation' where sid =" . $id;
+		$result = mysqli_query($connection, $query);
+		
+			echo "<p class='confirmmsg'>Location has been updated</p>";
+
+
+	}
+
+	//$selectlocation = (!empty($_POST['selectlocation']) ? $connection->real_escape_string($_POST['selectlocation']) : false);
+
+	if (isset($id)) {
+		  	
+		echo "<p><span class='bold'>Report ID: </span>".$id."</p>";
+
+		$selection="select o.species as Species, s.locationName as Location, o.OrganismName as Organism_Name, o.Habitat, ov.PrimaryColor as Color, ov.Rarity";
+		$from=" from sighting_report s, organism o, organism_variation ov";
+		$projection=" where s.sid = ".$id." and s.species = o.species and ov.species = o.species";
+		$query = "";
+		if(checkOrganismType("animal", $connection, $id)){
+			$selection.=", a.PrimaryDiet as Diet, a.Aggresiveness, a.health, a.sound";
+		  	$from.=", animal a";
+		  	$projection.=" and a.species = o.species";
+		  	$query = $query.$selection.$from.$projection;
+		}else if(checkOrganismType("plant", $connection, $id)){
+			$selection.=", p.FlowerColor as Flower_Color, p.Calories as Calories_per100g, fr.FruitName as Fruit_Name, fr.Calories as Fruit_Calories_per100g";
+		  	$from.=", plant p left join plant_Fruit fr on p.species = fr.species";
+		  	$projection.=" and p.species = o.species";
+		  	$query = $query.$selection.$from.$projection;
+
+		}else if(checkOrganismType("fungus", $connection, $id)){
+
+			$selection.=", f.Edibility, f.Texture, f.FungusSize, f.smell, t.chemical, t.toxicity, t.onset, t.treatment";
+		  	$from.=", fungus f left join produces_Toxin t on f.species = t.species";
+		  	$projection.=" and f.species = o.species";
+		  	$query = $query.$selection.$from.$projection;
+		}
+
+		$result = mysqli_query($connection, $query);
+
+		if(mysqli_num_rows($result) > 0){
+			
+			printKeyVal($result);
+		}
+	}
+
+?>
+
+
+	<form id="editSighting" method="POST" action="<?php if(isset($_GET['user']) && isset($_GET['sid'])){echo 'edit-single.php?sid='.$id.'&user='. $_GET['user'];} ?>">
+		<h3>Edit Organism/Location</h3>
+		<div>
+	      	<label>Organism</label>
+	        <?php 
+	        	$query="select species, OrganismName from organism";
+	        	$result = mysqli_query($connection, $query);
+	        	printDropdown($result, "selectspecies");
+	        ?>
+	        <input class ='button' type='submit' value='Update' name='updateorganism'>;
+	    </div>
+	    <div>
+	        <label>Location</label>
+	        <?php 
+	        	$query="select locationName from location";
+	        	$result = mysqli_query($connection, $query);
+	        	printDropdown($result, "selectlocation");
+	        ?>
+	        <input class ='button' type='submit' value='Update' name='updatelocation'>;
+	    </div>
+	</form>	
+
+<div class="resultposition"></div>
+
+<a href="<?php if(isset($_GET['user'])){echo 'edit-delete.php?user='. $_GET['user'];} ?>">Back to Edit/Delete</a>
 <?php include "footer-user.php"; ?>
--- your-account.php
+++ your-account.php
@@ -1,118 +1,46 @@
 <?php include "header.php"; ?>
 
-	<h2>Your Account</h2>
+<h2>Your Account</h2>
 
-	<!-- ******** Add message to confirm successful update *********-->
-
-	<form id="updateAccountForm" method="POST" action="your-account.php">
-		<div>
-			<label>Current Email*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New Email</label>
-			<input type="text" name="OldEmail"><input type="text" name="NewEmail">
-		</div>
-		<div>
-			<label>Current Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New Name</label>
-			<input type="text" name="OldName"><input type="text" name="NewName">
-		</div>
-		<div>
-			<label>Current Password&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New Password</label>
-			<input type="text" name="OldPassword"><input type="text" name="NewPassword">
-		</div>
-
-		<p class="remark">* indicates required field</p>
-
-		<input class ="button" type="submit" value="Update" name="updateaccountsubmit">
-
-	</form>
-
-	<?php
+<?php
+	
 	include('connect.php');
+	include('phpfunction.php');
 
-	if (isset ($_POST['updateaccountsubmit'])) {
-		$oldemail = (!empty($_POST['OldEmail']) ? $connection->real_escape_string($_POST['OldEmail']) : false);
-		$newemail = (!empty($_POST['NewEmail']) ? $connection->real_escape_string($_POST['NewEmail']) : false);
-		$newname = (!empty($_POST['NewName']) ? $connection->real_escape_string($_POST['NewName']) : false);
-		$newpassword = (!empty($_POST['NewPassword']) ? $connection->real_escape_string($_POST['NewPassword']) : false);
+	$userid = $_GET["user"];
 
-		if ($oldemail) {
-			echo $oldemail;
-			if ($newemail) {
-				echo $newemail;
-				$updateemail = "UPDATE user SET email = '$newemail' WHERE email = '$oldemail'";
-				echo $updateemail;
-				$emailresult = mysqli_query($connection, $updateemail);
-				if ($emailresult) {
-					echo "Email updated successfully!";
-				} else {
-					echo "Couldn't issue database query";
-					echo mysqli_error($connection);
-				}
-			}
 
-			if ($newname) {
-				echo $newname;
-				$updatename = "UPDATE user SET name = '$newname' WHERE email = '$oldemail'";
-				$nameresult = mysqli_query($connection, $updatename);
-				if ($nameresult) {
-					echo "Name updated successfully!";
-				} else {
-					echo "Couldn't issue database query";
-					echo mysqli_error($connection);
-				}
-			}
+	if(isset($_POST['updatesubmit'])){
 
-			if ($newpassword) {
-				echo $newpassword;
-				$updatepass = "UPDATE user SET password = '$newpassword' WHERE email = '$oldemail'";
-			 	$passresult = mysqli_query($connection, $updatepass);
-				if ($passresult) {
-					echo "Password updated successfully!";
-				} else {
-					echo "Couldn't issue database query";
-					echo mysqli_error($connection);
-				}
-			}
+		$name = (!empty($_POST['Name']) ? $connection->real_escape_string($_POST['Name']) : false);  
+		$email = (!empty($_POST['Email']) ? $connection->real_escape_string($_POST['Email']) : false);       
+		$password = (!empty($_POST['Password']) ? $connection->real_escape_string($_POST['Password']) : false); 
+		
 
-			} else {
-				echo "Please input your current email";
-			}
-				mysqli_close($connection);
+		if($name && $email && $password){
+			echo "<p class='confirmmsg'>Your account info is updated</p>";
+		}else{
+			echo "<p class='confirmmsg'>Name, Email and Password cannot be left blank.</p>";
 		}
 
+		$query="update user set name='$name', email='$email', password='$password' where userid=2";
+		$result = mysqli_query($connection, $query);
+
+	}
+
+	if (isset($userid)) {
+		  	
+		$query = "select name as Name, email as Email, password as Password from user where userid = ".$userid;
+
+		$result = mysqli_query($connection, $query);
+
+		if(mysqli_num_rows($result) > 0){
+			printResultFormAllEdit($result, 'your-account.php?&user='.$userid);
+		}
+	}
 
 
+?>
 
-
-
-			// echo $oldemail;
-			// $selection = "SELECT * FROM user WHERE email='$oldemail'";
-			// echo $selection;
-			// $result = mysqli_query($connection, $selection);
-			// if ($result) {
-			// 	//$udpatequery = "UPDATE user SET "
-			// 	echo "User exsits!";
-			// 	// echo '<table align ="left"
-			// 	// 			cellspacing = "5" cell padding = "8">
-			// 	// 			<tr>
-			// 	// 			<td align '
-		// 	} else {
-		// 		echo "Couldn't issue database query";
-		// 		echo mysqli_error($connection);
-		// 	}
-		// } else {
-		// 	echo "Please input your current email";
-		// }
-		// mysqli_close($connection);
-
-
-	 ?>
-
-	<p class="remark">display user ID and date join here</p>
-
-
+<div class="resultposition"></div>
 <?php include "footer-user.php"; ?>
