6e9aeafbcfcc080429ea5c48bd4b41aeab463dad
nawa-ton <kungcheow@gmail.com>
2019-06-19 11:49:30 -0700

    admin-users-account is working
    Still not add $_SESSION to load user login data

--- admin-users-account.php
+++ admin-users-account.php
@@ -1,59 +1,102 @@
 <?php include "header.php"; ?>
-
 	<h2>Users Account</h2>
-
-
-
-	<!-- ********* Add message to confirm successful deletion ******** -->
-		<form id="searchUsersForm" method="POST" action="index.php">
+	<form id="searchUsersForm" method="POST" action="admin-users-account.php">
 		<div>
+	      <label>Search by</label>
+	      <select id="searchuserby" name="searchuserby">
+	        <option value="searchall" name="searchall" selected="selected">Show All</option>
+	        <option value="UserID" name="UserID">User ID</option>
+	        <option value="NameYear" name="NameYear">Name and/or join year</option>
+	      </select>
+	    </div>
+		<div id = "searchbyUserID" class="searchuser">
 			<label>User ID</label>
 			<input type="text" name="UserID">
 		</div>
 
-		<div>
+		<div id = "searchbyName" class="searchuser">
 			<label>Name</label>
 			<input type="text" name="Name">
 		</div>
 
-		<div>
-			<label>Year Joined</label>
-			<input type="text" name="YearJoined">
+		<div id = "searchbyJoinYear" class="searchuser">
+			<label>Join Year</label>
+			<input type="text" name="JoinYear">
 		</div>
 
 		<input class ="button" type="submit" value="Submit" name="searcheusersubmit">
 	</form>	
 
 
-	<div class="viewResults">
+<?php
+	
+	include('connect.php');
+	include('phpfunction.php');
 
-	<!-- ******** This is just a placeholder table. To get data from the database, refer to function printResult in oracle-test.php (tutorial 7) ******** -->
-		<p class="remark">Note: This is only to give the idea about layout of the table. The actual table shold be appeared only after submitting query</p>
+	$columnNames = array("User ID", "Name","Email", "Join date");
 
-	<table>
-		<thead>
-			<tr>
-				<th>User ID</th>
-				<th>Name</th>
-				<th>Email</th>
-				<th>Date Joined</th>
-				<th></th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td>Placeholder</td>
-				<td>Placeholder</td>
-				<td>Placeholder</td>
-				<td>Placeholder</td>
-				<td><a hred="users-account.php">Delete</a></td> 
-			</tr>
-			<tr>
+	if(isset($_POST['searcheusersubmit'])){
 
-			</tr>
-		</tbody>
+		$UserID = (!empty($_POST['UserID']) ? $connection->real_escape_string($_POST['UserID']) : false);
+		$Name = (!empty($_POST['Name']) ? $connection->real_escape_string($_POST['Name']) : false);
+		$JoinYear = (!empty($_POST['JoinYear']) ? $connection->real_escape_string($_POST['JoinYear']) : false);
 
-	</table>
+		$selection="";
+		$query="";
+		if($UserID){
+			$selection.=" where userid = $UserID";
+		}
+
+		if($Name && $JoinYear){
+			$selection.=" where name LIKE '%$Name%' AND DATE_FORMAT(joindate, '%d/%m/%Y') LIKE '%$JoinYear%'";
+		}
+
+		if($Name && !$JoinYear){
+			$selection.=" where name LIKE '%$Name%'";
+		}
+
+		if(!$Name && $JoinYear){
+			$selection.=" WHERE DATE_FORMAT(joindate, '%d/%m/%Y') LIKE '%$JoinYear%'";
+		}
+
+		$query = "select userid, name, email, joindate from user";
+
+		$query.=$selection;
+
+		$result = mysqli_query($connection, $query);
+		
+		if(mysqli_num_rows($result) > 0){
+			printUserTable($result, $columnNames);
+			echo "<p class='confirmmsg'>" . mysqli_num_rows($result) . " results found</p>";
+		}else{
+			echo "<p class='confirmmsg'>No result found</p>";
+		}
+		
+		
+
+
+	}
+
+	if (isset($_GET["user"])) {
+		$user = $_GET["user"];
+
+		$query ="delete from user where UserID=" . $user;
+
+		$result = mysqli_query($connection, $query);
+		
+		$query = "select userid, name, email, joindate from user";
+
+		$result = mysqli_query($connection, $query);
+
+		printUserTable($result, $columnNames);
+		echo "<p class='confirmmsg'>User ID " . $user . " has been deleted</p>";
+	}
+
+	mysqli_close($connection);
+
+?>
+
+
 
 
 <?php include "footer-user.php"; ?>
