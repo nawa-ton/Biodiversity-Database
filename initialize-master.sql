95df07e1967e2706ae3e21af19a92fa2728510e1
June-Wu <junew5x@hotmail.com>
2019-06-19 13:06:00 -0700

    Update initialize-master.sql

--- initialize-master.sql
+++ initialize-master.sql
@@ -253,8 +253,6 @@ insert into Organism_Variation values ('Equus magicalus', 'white', 'rare');
 
 insert into Organism_Variation values ('Equus magicalus', 'rainbow', 'rare');
 
-insert into Organism_Variation values ('Equus magicalus', 'grey and black', 'rare');
-
 insert into Organism_Variation values ('Equus magicalus', 'reddish', 'rare');
 
 insert into Organism_Variation values ('Equus magicalus', 'black', 'rare');
