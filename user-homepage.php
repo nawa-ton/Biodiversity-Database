8d1fc71dcb279e4bc27dd52950904cd2e49b6c45
June-Wu <junew5x@hotmail.com>
2019-06-19 10:22:45 -0700

    added query layout

--- user-homepage.php
+++ user-homepage.php
@@ -12,6 +12,7 @@
 	<form id="viewForm" method="POST" action="user-homepage.php">
 
 		<div>
+			<div id="selecttype" >
 			<label>Organism Type</label>
 			<select id="vieworganismtype" name="vieworganismtype">
 			   	<option value="all" selected="selected">All</option>
@@ -19,22 +20,23 @@
 			    <option value="plant">Plant</option>
 			    <option value="fungus">Fungus</option>
 			</select>
+			</div>
 		</div>
 		<div>
 			<label>Query Type</label>
 			<select id="querytype" name="querytype">
 			   	<option value="selection" selected="selected">Selection</option>
 			   	<option value="aggregation">Aggregation</option>
+				<option value="division">Division</option>
 			</select>
 		</div>
-			<div id = "insertselection" parent="querytype">
+			<div id = "insertselection">
 				<label>Show Columns</label>
 				<input type="checkbox" name="cspecies" checked>Species		
 				<input type="checkbox" name="cname" checked>Name		
 				<input type="checkbox" name="chabitat" checked> Habitat		
 				<input type="checkbox" name="cprimarycolor" checked> Primary Color
-				<input type="checkbox" name="crarity" checked> Rarity<br><br>	
-
+				<input type="checkbox" name="crarity" checked> Rarity<br><br>
 				<div>
 					<label>Organism Name</label>
 					<input type="text" name="OrganismName">
@@ -57,14 +59,25 @@
 				</div>
 			</div>
 
-			<div id="insertaggregation" parent="querytypw">
+			<div id="insertaggregation" >
 				<label>Group By</label>
 				<select id="group" name="group">
-			   	<option value="gspecies" selected="selected">Species</option>
-			   	<option value="ghabitat">Habitat</option>
-				<option value="gprimarycolor">PrimaryColor</option>
-				<option value="grarity">Rarity</option>
-			</select>	
+					<option name="gspecies" value="species" selected="selected">Species</option>
+					<option name="gorganismtype" value="organismname">Name</option>
+					<option name="ghabitat" value="habitat">Habitat</option>
+					<option name="gprimarycolor" value="primarycolor">PrimaryColor</option>
+					<option name="grarity" value="rarity">Rarity</option>
+				</select>	
+			</div>
+
+			<div id="insertdivision" >
+					<label>Find Animals that Eats All</label>
+					<select id="typeeaten" name="typeeaten">
+						<option name="eorganism" value="organism" selected="selected">Organisms</option>
+						<option name="eanimal" value="animal">Animals</option>
+						<option name="eplant" value="plant">Plants</option>
+						<option name="efungus" value="fungus">Fungus</option>
+					</select>	
 			</div>
 		</div>
 
