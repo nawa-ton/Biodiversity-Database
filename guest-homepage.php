6dca32d4db9b570623069e6c13bd341189787484
June-Wu <junew5x@hotmail.com>
2019-06-19 10:23:50 -0700

    added query layout

--- guest-homepage.php
+++ guest-homepage.php
@@ -7,8 +7,8 @@
 	<h2>Search Organism</h2>
 		
 	<form id="viewForm" method="POST" action="guest-homepage.php">
-
 		<div>
+			<div id="selecttype" >
 			<label>Organism Type</label>
 			<select id="vieworganismtype" name="vieworganismtype">
 			   	<option value="all" selected="selected">All</option>
@@ -16,15 +16,17 @@
 			    <option value="plant">Plant</option>
 			    <option value="fungus">Fungus</option>
 			</select>
+			</div>
 		</div>
 		<div>
 			<label>Query Type</label>
 			<select id="querytype" name="querytype">
 			   	<option value="selection" selected="selected">Selection</option>
 			   	<option value="aggregation">Aggregation</option>
+				<option value="division">Division</option>
 			</select>
 		</div>
-			<div id = "insertselection" parent="querytype">
+			<div id = "insertselection">
 				<label>Show Columns</label>
 				<input type="checkbox" name="cspecies" checked>Species		
 				<input type="checkbox" name="cname" checked>Name		
@@ -53,18 +55,27 @@
 				</div>
 			</div>
 
-			<div id="insertaggregation" parent="querytypw">
+			<div id="insertaggregation" >
 				<label>Group By</label>
 				<select id="group" name="group">
-			   	<option name="gspecies" value="species" selected="selected">Species</option>
-				<option name="gorganismtype" value="organismname">Name</option>
-			   	<option name="ghabitat" value="habitat">Habitat</option>
-				<option name="gprimarycolor" value="primarycolor">PrimaryColor</option>
-				<option name="grarity" value="rarity">Rarity</option>
-			</select>	
+					<option name="gspecies" value="species" selected="selected">Species</option>
+					<option name="gorganismtype" value="organismname">Name</option>
+					<option name="ghabitat" value="habitat">Habitat</option>
+					<option name="gprimarycolor" value="primarycolor">PrimaryColor</option>
+					<option name="grarity" value="rarity">Rarity</option>
+				</select>	
+			</div>
+
+			<div id="insertdivision" >
+					<label>Find Animals that Eats All</label>
+					<select id="typeeaten" name="typeeaten">
+						<option name="eorganism" value="organism" selected="selected">Organisms</option>
+						<option name="eanimal" value="animal">Animals</option>
+						<option name="eplant" value="plant">Plants</option>
+						<option name="efungus" value="fungus">Fungus</option>
+					</select>	
 			</div>
 		</div>
-		
 		<input class ="button" type="submit" value="Submit" name="organismsubmit">
 	</form>
 <?php include "organism-query.php";?>
