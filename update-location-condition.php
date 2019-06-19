6488c5cb05724d499cc83bee50f2a344bafb8738
smarlowe <51905271+smarlowe@users.noreply.github.com>
2019-06-18 19:19:28 -0700

    Add files via upload
    not completed/not working

--- update-location-condition.php
+++ update-location-condition.php
@@ -8,12 +8,12 @@
 	<form id="updateLocationForm" method="POST" action="update-location-condition.php">
 		<div>
 			<label>Location Name</label>
-			<input type="text" name="Location_Name">
+			<input type="text" name="LocationName">
 		</div>
 
 		<div>
 			<label>Location Condition</label>
-			<select id="locationcondition" name="locationcondition">
+			<select id="locationcondition" name="LocationCondition">
 				<option value="normal" selected="selected">Normal</option>
 			   	<option value="construction">Construction</option>
 			    <option value="maintenance">Maintenance</option>
@@ -46,9 +46,47 @@
 				<input type="text" name="Task">
 			</div>
 		</div> <!-- end div insertmaintenance-->
- 
+
 		<input class ="button" type="submit" value="Update" name="updatesubmit">
 </form>
 
-		
+<?php
+
+include('connect.php');
+
+if (isset($_POST['updatesubmit'])) {
+	$locationcondition = $_POST['LocationCondition'];
+	$locationname = (!empty($_POST['LocationName']) ? $connection->real_escape_string($_POST['LocationName']) : false);
+	if ($locationcondition == "normal") {
+		$maintenancequery = "DELETE FROM location_maintenance WHERE 1=1";
+		if ($locationname) {
+			//delete record from location_maintenance
+			$maintenancequery.= " AND locationname = '%locationname%'";
+			$maintenanceresponse = mysqli_query($connection, $maintenancequery);
+			if ($maintenanceresponse) {
+				echo "Location_Maintenace table updated successfully!";
+			} else {
+				echo "Couldn't issue database query";
+			}
+
+			//delete record from location_remodel
+			$remodelquery = "DELETE FROM location_remodel WHERE 1=1";
+			$remodelquery.=" AND locationname = '%locationname'";
+			$remodelresponse = mysqli_query($connection, $remodelquery);
+			if ($remodelresponse) {
+				echo "Location_remodel table updated successfully!";
+			} else {
+				echo "Couldn't issue database query";
+			}
+		}
+	} else
+		if ($locationcondition == "construction") {
+			//update fields
+		} else
+			if ($locationcondition == "maintenance") {
+				//update fields
+			}
+}
+ ?>
+
 <?php include "footer-user.php"; ?>
